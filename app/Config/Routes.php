37400e577ae4bee2dd9bc954b539f08b6fe6921f
rahmadelya88 <111202415729@mhs.dinus.ac.id>
2026-04-26 23:53:55 +0700

    membuat branch baru untuk melanjutkan tugas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,13 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
 $routes->get('/', 'Home::index');
-$routes->get('/produk', 'ProdukController::index');
-$routes->get('/keranjang', 'TransaksiController::index');
+
+$routes->get('login', 'AuthController::login');
+$routes->post('login', 'AuthController::login');
+$routes->get('logout', 'AuthController::logout');
+
+$routes->get('produk', 'ProdukController::index', ['filter' => 'auth']);
+$routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
+$routes->get('/profile', 'ProfileController::index', ['filter' => 'auth']);
--- app/Views/layout_clear.php
+++ app/Views/layout_clear.php
@@ -44,48 +44,12 @@ if(uri_string()!=""){
   ======================================================== -->
 </head>
 
-<body>
-
-  <?= $this->include('components/header') ?>
-
-  <?= $this->include('components/sidebar') ?>
-
-  <main id="main" class="main">
-
-    <div class="pagetitle">
-      <h1>Data Tables</h1>
-      <nav>
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item">Home</li>
-            <?php
-	        if($hlm!="Home"){
-	        ?>
-	        <li class="breadcrumb-item"><?php echo $hlm?></li> 
-	        <?php
-	        }
-            ?> 
-        </ol>
-      </nav>
-    </div><!-- End Page Title -->
-
-    <section class="section">
-      <div class="row">
-        <div class="col-lg-12">
-
-          <div class="card">
-            <div class="card-body">
-                <h5 class="card-title"><?php echo $hlm?></h5>
-                <?= $this->renderSection('content') ?>
-            </div>
-          </div>
-
-        </div>
-      </div>
-    </section>
-
-  </main><!-- End #main -->
-
-  <?= $this->include('components/footer') ?>
+<body> 
+	<main>
+	
+	  <?= $this->renderSection('content') ?> 
+	  
+	</main><!-- End #main -->
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
