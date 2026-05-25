de834409c842e2d3b9462963cb855b7112bd8192
rahmadelya88 <111202415729@mhs.dinus.ac.id>
2026-05-25 15:44:08 +0700

    Menambahkan read validation

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -5,19 +5,34 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 
+use App\Models\UserModel; 
+
 class AuthController extends BaseController
 {
+    protected $userModel;
+
     function __construct()
-{
-    helper('form');
-}
+    {
+        helper('form');
+        $this->userModel = new UserModel();
+    }
 
 public function login()
 {
     if ($this->request->getPost()) {
+    $rules = [
+        'username' => 'required|min_length[6]',
+        'password' => 'required|min_length[7]|numeric',
+    ];
+
+    if ($this->validate($rules)) {
         $username = $this->request->getVar('username');
         $password = $this->request->getVar('password');
 
+        $dataUser = $this->userModel ->where(['username' => $username])->first();
+
+        if ($dataUser) {
+	        if (password_verify($password, $dataUser['password'])) {
         $dataUser = [
                         'username' => 'Rahma', 
                         'password' => '202cb962ac59075b964b07152d234b70', // passw 123
@@ -44,6 +59,10 @@ public function login()
             session()->setFlashdata('failed', 'Username Tidak Ditemukan');
             return redirect()->back();
         }
+    } else {
+        session()->setFlashdata('failed', $this->validator->listErrors());
+        return redirect()->back();
+    }
     } else {
         return view('v_login');
     }
