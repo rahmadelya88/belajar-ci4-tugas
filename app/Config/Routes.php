4170da7e220d08aa51d720f112df24ffdcf3bc2e
rahmadelya88 <111202415729@mhs.dinus.ac.id>
2026-06-22 20:37:20 +0700

    Menambahkan data transaksi pembelian

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -25,7 +25,11 @@ $routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
     $routes->post('edit', 'TransaksiController::cart_edit');
     $routes->get('delete/(:any)', 'TransaksiController::cart_delete/$1');
     $routes->get('clear', 'TransaksiController::cart_clear');
+    $routes->get('checkout', 'TransaksiController::checkout');
 });
 
-$routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
-$routes->get('/profile', 'ProfileController::index', ['filter' => 'auth']);
+$routes->get('checkout', 'TransaksiController::checkout', ['filter' => 'auth']);
+$routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
+
+$routes->get('ajax/destinations','TransaksiController::destinations', ['filter' => 'auth']);
+$routes->get('ajax/costs','TransaksiController::costs', ['filter' => 'auth']);
--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -4,7 +4,6 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
-
 use App\Models\UserModel; 
 
 class AuthController extends BaseController
@@ -17,60 +16,68 @@ class AuthController extends BaseController
         $this->userModel = new UserModel();
     }
 
-public function login()
-{
-    if ($this->request->getPost()) {
-    $rules = [
-        'username' => 'required|min_length[6]',
-        'password' => 'required|min_length[7]|numeric',
-    ];
+    public function login()
+    {
+        if ($this->request->getPost()) {
+            $rules = [
+                'username' => 'required|min_length[6]',
+                'password' => 'required|min_length[7]|numeric',
+            ];
 
-    if ($this->validate($rules)) {
-        $username = $this->request->getVar('username');
-        $password = $this->request->getVar('password');
+            if ($this->validate($rules)) {
+                $username = $this->request->getVar('username');
+                $password = $this->request->getVar('password');
 
-        $dataUser = $this->userModel ->where(['username' => $username])->first();
+                $dataUser = $this->userModel->where(['username' => $username])->first();
 
-        if ($dataUser) {
-	        if (password_verify($password, $dataUser['password'])) {
-        $dataUser = [
-                        'username' => 'Rahma', 
-                        'password' => '202cb962ac59075b964b07152d234b70', // passw 123
-                        'role' => 'admin', 
-                        'email' => '111202415729@mhs.dinus.ac.id' 
+                if ($dataUser) {
+                    if (password_verify($password, $dataUser['password'])) {
+                        $dataUser = [
+                            'username' => 'Rahmadlya', 
+                            'password' => '6e5f603247a61d89625127f3f67836a7', // passw 12345678
+                            'role'     => 'admin', 
+                            'email'    => '111202415729@mhs.dinus.ac.id' 
                         ]; 
 
-        if ($username == $dataUser['username']) {
-            if (md5($password) == $dataUser['password']) {
-                session()->set([
-                    'username' => $dataUser['username'],
-                    'role' => $dataUser['role'],
-                    'email' => '111202415729@mhs.dinus.ac.id',
-                    'login_time' => date('Y-m-d H:i:s'),
-                    'isLoggedIn' => TRUE
-                ]);
+                        if ($username == $dataUser['username']) {
+                            if (md5($password) == $dataUser['password']) {
+                                session()->set([
+                                    'username'   => $dataUser['username'],
+                                    'role'       => $dataUser['role'],
+                                    'email'      => '111202415729@mhs.dinus.ac.id',
+                                    'login_time' => date('Y-m-d H:i:s'),
+                                    'isLoggedIn' => TRUE
+                                ]);
 
-                return redirect()->to(base_url('/'));
+                                return redirect()->to(base_url('/'));
+                            } else {
+                                session()->setFlashdata('failed', 'Username & Password Salah');
+                                return redirect()->back();
+                            }
+                        } else {
+                            session()->setFlashdata('failed', 'Username Tidak Ditemukan');
+                            return redirect()->back();
+                        }
+                    } else {
+                        session()->setFlashdata('failed', 'Username & Password Salah');
+                        return redirect()->back();
+                    }
+                } else {
+                    session()->setFlashdata('failed', 'Username Tidak Ditemukan');
+                    return redirect()->back();
+                }
             } else {
-                session()->setFlashdata('failed', 'Username & Password Salah');
+                session()->setFlashdata('failed', $this->validator->listErrors());
                 return redirect()->back();
             }
         } else {
-            session()->setFlashdata('failed', 'Username Tidak Ditemukan');
-            return redirect()->back();
+            return view('v_login');
         }
-    } else {
-        session()->setFlashdata('failed', $this->validator->listErrors());
-        return redirect()->back();
     }
-    } else {
-        return view('v_login');
-    }
-}
 
-public function logout()
-{
-    session()->destroy();
-    return redirect()->to('login');
-}
+    public function logout()
+    {
+        session()->destroy();
+        return redirect()->to('login');
+    }
 }
